101b2386f53e5dba6487b30268d44e031b202a60
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-14 03:57:17 -0300

    add base to index js called functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log("Dev m3");
+import { getApiItems } from './getApiItems.js';
+import { addEventsElements } from './addEventsElements.js';
+import { showItemsScreen } from './showItemsScreen';
+import { cartNumberRender } from './cartNumberRender.js';
+
+let TOT_ITEMS_PAGE = 3;
+
+
+localStorage.setItem('TOT_ITEMS_CART', 0);
+
+
+getApiItems(TOT_ITEMS_PAGE);
+cartNumberRender()
+
+addEventsElements(TOT_ITEMS_PAGE, showItemsScreen)
+
+
+
