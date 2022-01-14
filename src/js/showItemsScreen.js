e4d9d328cf494ef2345df3120c5d708b5bf91d95
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-14 03:59:03 -0300

    add resposibity in schow itens to create itens

--- src/js/showItemsScreen.js
+++ src/js/showItemsScreen.js
@@ -1,10 +1,22 @@
-export  function showItemsScreen(listItems) {
+import { createProduct } from './createProduct';
+
+
+export  function showItemsScreen(TOT_ITEMS_PAGE) {
     const $productlist = document.querySelector('.menu__produtsList')
-    
+    const listItems = JSON.parse(localStorage.getItem('productsList'));
+    const totDados = listItems.length;
+    const  products = [];
+
+    for (let i = 0; i < totDados; i++) {
+
+        if (i >= TOT_ITEMS_PAGE) break;
+
+        products.push(createProduct(listItems[i]))
+    }
+
     $productlist.innerHTML = '';
 
-    console.log(listItems);
-    for (let element of listItems) {
+    for (let element of products) {
         $productlist.appendChild(element)
     }
 }
