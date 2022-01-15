a8aeb6d2009654db5eb65cc9103ad0dea5886ea4
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-14 22:47:57 -0300

    add possibity to Mark off input price

--- src/js/addEventsElements.js
+++ src/js/addEventsElements.js
@@ -7,10 +7,13 @@ export function addEventsElements (TOT_ITEMS_PAGE) {
     const $moreItems = document.querySelector('.menu__moreItems');  
     const $orderSelect = document.querySelector('.menu__order')
     const $form = document.querySelector('.filters__form');
+    const items_at_a_time = +localStorage.getItem('ITEMS_AT_A_TIME');
+    const $wrapperPrices = document.querySelectorAll('.form__session--options input');
+    let ultimoElementoMarcado = ''
 
     $moreItems.addEventListener('click',  () => {
-        const items_at_a_time = +localStorage.getItem('ITEMS_AT_A_TIME');
-        console.log(TOT_ITEMS_PAGE)
+        
+        
         TOT_ITEMS_PAGE += items_at_a_time;
         showItemsScreen(TOT_ITEMS_PAGE)
     })
@@ -31,38 +34,66 @@ export function addEventsElements (TOT_ITEMS_PAGE) {
         const productslistOrdered = productlist.sort(rulesOrder[typeOrder])
 
         localStorage.setItem('productsList', JSON.stringify(productslistOrdered));
-        TOT_ITEMS_PAGE = 3;
+        TOT_ITEMS_PAGE = items_at_a_time;
         showItemsScreen(TOT_ITEMS_PAGE)
     })
 
-    $form.addEventListener('change', async ()=> {
-        const filters = getDataFormFilters()
-        const listItems = JSON.parse(localStorage.getItem('AllproductsList'));
-        
-        const listItemsFiltered = listItems.filter(item => {
-            if (filters.colors.length > 0) {
-                if (!filters.colors.includes(item.color.toLowerCase())) return false
-            }
-            if (filters.sizes.length > 0) {
-                if (!filters.sizes.includes(item.size.toLowerCase())) return false
-            }
-            if (filters.price.length > 0) {
-                if (!filters.price.includes(item.price.toLowerCase())) return false
-            }
-            return true
+    $form.addEventListener('change', ()=> handleFiltersChange(TOT_ITEMS_PAGE, items_at_a_time))
+
+    $wrapperPrices.forEach((input) => {
+
+        input.addEventListener('click', (e)=> {
+             const valorElemento = e.target.value
+             if (ultimoElementoMarcado === valorElemento) {
+                 e.target.checked = false
+                 ultimoElementoMarcado = ''
+
+                 TOT_ITEMS_PAGE = items_at_a_time;
+                 handleFiltersChange(TOT_ITEMS_PAGE, items_at_a_time)
+
+             }
+             else {
+                ultimoElementoMarcado = valorElemento
+             }
+            
         })
-        console.log(listItemsFiltered)
-
-        await localStorage.setItem('productsList', JSON.stringify(listItemsFiltered));
-
-        TOT_ITEMS_PAGE = 3;
-        showItemsScreen(3)
-    } )
+    })
 
 
 
     
 }
+
+async function handleFiltersChange (TOT_ITEMS_PAGE, items_at_a_time) {
+    const filters = getDataFormFilters()
+    const listItems = JSON.parse(localStorage.getItem('AllproductsList'));
+    
+    console.log(filters)
+
+    const listItemsFiltered = listItems.filter(item => {
+        if (filters.colors.length > 0) {
+            if (!filters.colors.includes(item.color.toLowerCase())) return false
+        }
+        if (filters.sizes.length > 0) {
+            
+            const has = item.size.find(size => filters.sizes.includes(size.toLowerCase()))
+           
+            if (!has) return false
+        }
+        if (filters.price !== undefined) {
+            const [minNumber, maxNumber] = filters.price.split('-')
+            
+            if (item.price < minNumber || item.price > maxNumber) return false
+   
+        }
+        return true
+    })
+
+    await localStorage.setItem('productsList', JSON.stringify(listItemsFiltered));
+
+    TOT_ITEMS_PAGE = items_at_a_time;
+    showItemsScreen(TOT_ITEMS_PAGE)
+} 
 
 
 
