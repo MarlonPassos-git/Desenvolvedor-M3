eb0a778052eec82e4a6b8c662285080808eedb39
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 13:59:43 -0300

    modifying function form to receive class

--- src/js/getDataFormFilters.js
+++ src/js/getDataFormFilters.js
@@ -1,5 +1,6 @@
-export function getDataFormFilters() {
-    const $form = document.querySelector('.filters__form');
+export function getDataFormFilters(cssSelector) {
+    console.log(cssSelector)
+    const $form = document.querySelector(cssSelector);
     const $wrapperColors = $form.querySelectorAll('.form__session--colors input');
     const $wrapperSizes = $form.querySelectorAll('.form__session--sizes input');
     const $wrapperPrices = $form.querySelectorAll('.form__session--prices input');
