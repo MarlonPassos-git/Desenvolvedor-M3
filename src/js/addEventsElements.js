fab3db20c78d4c0e7884ad700d1b0a008767412e
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 13:56:59 -0300

    add all event elements in modal

--- src/js/addEventsElements.js
+++ src/js/addEventsElements.js
@@ -6,10 +6,15 @@ export function addEventsElements (TOT_ITEMS_PAGE) {
     
     const $moreItems = document.querySelector('.menu__moreItems');  
     const $orderSelect = document.querySelector('.menu__order')
-    const $form = document.querySelector('.filters__form');
+    const $formFilters = document.querySelector('.filters__form');
     const items_at_a_time = +localStorage.getItem('ITEMS_AT_A_TIME');
     const $wrapperPrices = document.querySelectorAll('.form__session--options input');
-    let ultimoElementoMarcado = ''
+    const $wrapperColors = document.querySelector('.form__session--colors .form__session--options');
+    const $buttonAllColors = document.querySelector('.form__session--button')
+    const $buttonFilter = document.querySelector('.menu__bottom--filter');
+    const $buttonOrder = document.querySelector('.menu__bottom--ordenar');
+    const $buttonModalClose = document.querySelector('.modal__buttonClosed');
+    let ultimoElementoMarcado = ''  
 
     $moreItems.addEventListener('click',  () => {
         
@@ -38,7 +43,7 @@ export function addEventsElements (TOT_ITEMS_PAGE) {
         showItemsScreen(TOT_ITEMS_PAGE)
     })
 
-    $form.addEventListener('change', ()=> handleFiltersChange(TOT_ITEMS_PAGE, items_at_a_time))
+    $formFilters.addEventListener('change', ()=> handleFiltersChange(TOT_ITEMS_PAGE, items_at_a_time, '.filters__form'))
 
     $wrapperPrices.forEach((input) => {
 
@@ -59,16 +64,38 @@ export function addEventsElements (TOT_ITEMS_PAGE) {
         })
     })
 
+    $buttonAllColors.addEventListener('click', (event) => {
+        
+        event.preventDefault()
+        
+        
+        $wrapperColors.classList.remove('showFiveItems')
 
+        $buttonAllColors.hidden = true
 
+    })
+
+    $buttonFilter.addEventListener('click', (event) => {
+        pageLock()
+        toggleModal()
+        fillModal('filtrar', $formFilters)
+    })
     
+    $buttonOrder.addEventListener('click', (event) => {
+        pageLock()
+        toggleModal()
+        fillModal('Ordernar', $orderSelect, TOT_ITEMS_PAGE, items_at_a_time )
+    })
+
+    $buttonModalClose.addEventListener('click', (event) => {
+        toggleModal()
+        pageLock()
+    })
 }
 
-async function handleFiltersChange (TOT_ITEMS_PAGE, items_at_a_time) {
-    const filters = getDataFormFilters()
+async function handleFiltersChange (TOT_ITEMS_PAGE, items_at_a_time, cssSelector) {
+    const filters = getDataFormFilters(cssSelector)
     const listItems = JSON.parse(localStorage.getItem('AllproductsList'));
-    
-    console.log(filters)
 
     const listItemsFiltered = listItems.filter(item => {
         if (filters.colors.length > 0) {
@@ -89,6 +116,8 @@ async function handleFiltersChange (TOT_ITEMS_PAGE, items_at_a_time) {
         return true
     })
 
+    console.log(listItemsFiltered)
+
     await localStorage.setItem('productsList', JSON.stringify(listItemsFiltered));
 
     TOT_ITEMS_PAGE = items_at_a_time;
@@ -97,3 +126,75 @@ async function handleFiltersChange (TOT_ITEMS_PAGE, items_at_a_time) {
 
 
 
+function toggleModal () {
+    const $modal = document.querySelector('#modal');
+    
+    $modal.classList.toggle('modal');
+}
+
+function fillModal (title, content, TOT_ITEMS_PAGE, items_at_a_time) {
+    const $modalTitle = document.querySelector('.modal__title');
+    const $modalContent = document.querySelector('.modal__content');
+    const $cloneContent = content.cloneNode(true);
+    $modalTitle.innerHTML = title;
+
+    console.log($modalContent)
+
+    if (title == 'Ordernar') AddOrderEventsContent($cloneContent)
+    if (title == 'filtrar') AddFilterEventsContent($cloneContent, TOT_ITEMS_PAGE, items_at_a_time)
+    
+
+    $modalContent.innerHTML = '';
+    $modalContent.appendChild($cloneContent);
+}
+
+function pageLock() {
+    const $body = document.querySelector('body');
+    
+    $body.classList.toggle('lock');
+}
+
+function sortList (event) {
+    const typeOrder = event.target.value;
+            const productlist = JSON.parse(localStorage.getItem('productsList'));
+            const rulesOrder = {
+                ['menor_preco']: (a, b) => a.price - b.price,
+                ['maior_preco']: (a, b) => b.price - a.price,
+                ['mais_recentes']: (a, b) => {
+                    const c = new Date(a.date.replace(/\-/g, ','))
+                    const d = new Date(b.date.replace(/\-/g, ','))
+    
+                    return d - c
+                },
+            }
+            const productslistOrdered = productlist.sort(rulesOrder[typeOrder])
+    
+            localStorage.setItem('productsList', JSON.stringify(productslistOrdered));
+            
+            showItemsScreen(9)
+}
+
+function AddOrderEventsContent(content, TOT_ITEMS_PAGE, items_at_a_time){
+    const $select = content.querySelector('select')
+    const $optionDisabled = $select.querySelector('option[disabled]')
+
+    $optionDisabled.remove()
+    $select.size = 3;
+
+    content.addEventListener('click', (event) => {
+        
+        sortList (event)
+        toggleModal()
+        pageLock()
+    })
+}
+
+function AddFilterEventsContent(content, TOT_ITEMS_PAGE, items_at_a_time) {
+    content.addEventListener('click', ()=> {
+        handleFiltersChange(TOT_ITEMS_PAGE, items_at_a_time, '#modal form')
+    
+        
+    })
+
+    
+}
