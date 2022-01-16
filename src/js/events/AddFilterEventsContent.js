1ffe51b22f57c2d78ba64a5fef4abcf92907e359
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 15:52:08 -0300

    add event function 'AddFilterEventsContent.'

---
+++ src/js/events/AddFilterEventsContent.js
@@ -0,0 +1,8 @@
+import { handleFiltersChange } from "./handleFiltersChange"
+
+export function AddFilterEventsContent($content) {
+    $content.addEventListener('click', ()=> {
+        handleFiltersChange('#modal form')
+    
+    })
+}
