9018eff84deccec879f17afe28e12d78c8ca8397
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 15:10:06 -0300

    create function event 'showAllColorOptions'

---
+++ src/js/events/showAllColorOptions.js
@@ -0,0 +1,9 @@
+export function showAllColorOptions (event) {
+    event.preventDefault()
+
+    const $wrapperColors = document.querySelector('.form__session--colors .form__session--options');
+    const $buttonAllColors= event.target;
+
+    $wrapperColors.classList.remove('showFiveItems')
+    $buttonAllColors.hidden = true
+}
