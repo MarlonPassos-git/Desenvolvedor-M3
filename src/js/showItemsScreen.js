abc9c51cd9db8626439b27771826c286f4023ffa
marlon Felipe Passos <1marlonpassos@gmail.com>
2022-01-16 15:53:09 -0300

    refectoring totitem in scripts

--- src/js/showItemsScreen.js
+++ src/js/showItemsScreen.js
@@ -1,25 +1,23 @@
 import { createProduct } from './createProduct';
 
+export  function showItemsScreen() {
 
-export  function showItemsScreen(TOT_ITEMS_PAGE) {
-
+    const totItemsPage = +localStorage.getItem('TOT_ITEMS_PAGE');
     const $productlist = document.querySelector('.menu__produtsList')
     const listItems = JSON.parse(localStorage.getItem('productsList'));
     const totDados = listItems.length;
     const  products = [];
     const items_at_a_time = +localStorage.getItem('ITEMS_AT_A_TIME');
 
-    if (TOT_ITEMS_PAGE > totDados + items_at_a_time) return 
+    if (totItemsPage > totDados + items_at_a_time) return 
 
     for (let i = 0; i < totDados; i++) {
 
-        if (i >= TOT_ITEMS_PAGE) break;
+        if (i >= totItemsPage) break;
 
         products.push(createProduct(listItems[i]))
     }
 
     $productlist.innerHTML = '';
-
-    $productlist.append(...products)
-    
+    $productlist.append(...products) 
 }
